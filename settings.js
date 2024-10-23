c4cadfdb7594ebb66b428aa0ec5a47ba770265b3
senslowly <sennprtt@gmail.com>
2024-10-23 10:00:59 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,17 +9,17 @@ const chalk = require('chalk');
 
 //~~~~~~~~~~~~< GLOBAL SETTINGS >~~~~~~~~~~~~\\
 
-global.owner = ['6282113821188','6281356484612','6285773557984']
-global.packname = 'Bot WhatsApp'
-global.author = 'Nazedev'
-global.botname = 'Naze Bot'
+global.owner = ['6289697920805']
+global.packname = 'SenOfc'
+global.author = 'SennStranger AI'
+global.botname = 'SennStranger AI'
 global.listv = ['•','●','■','✿','▲','➩','➢','➣','➤','✦','✧','△','❀','○','□','♤','♡','◇','♧','々','〆']
 global.tempatDB = 'database.json'
 global.pairing_code = true
 
 global.fake = {
-	anonim: 'https://telegra.ph/file/95670d63378f7f4210f03.png',
-	thumbnailUrl: 'https://telegra.ph/file/fe4843a1261fc414542c4.jpg',
+	anonim: 'https://telegra.ph//file/ddbc5e8cb63be06133206.jpg',
+	thumbnailUrl: 'https://telegra.ph/file/db739d017a619aa7d89de.jpg',
 	thumbnail: fs.readFileSync('./src/media/naze.png'),
 	docs: fs.readFileSync('./src/media/fake.pdf'),
 	listfakedocs: ['application/vnd.openxmlformats-officedocument.spreadsheetml.sheet','application/vnd.openxmlformats-officedocument.presentationml.presentation','application/vnd.openxmlformats-officedocument.wordprocessingml.document','application/pdf'],
@@ -27,7 +27,7 @@ global.fake = {
 
 global.my = {
 	yt: 'https://youtube.com/c/Nazedev',
-	gh: 'https://github.com/nazedev',
+	gh: 'https://github.com/senslowly',
 	gc: 'https://chat.whatsapp.com/Hx9vcBVhbc04KLVGPFtH2R',
 	ch: '120363250409960161@newsletter',
 }
@@ -45,15 +45,15 @@ global.uang = {
 }
 
 global.mess = {
-	owner: 'Fitur Khusus Owner!',
-	admin: 'Fitur Khusus Admin!',
-	botAdmin: 'Bot Bukan Admin!',
-	group: 'Gunakan Di Group!',
-	private: 'Gunakan Di Privat Chat!',
-	prem: 'Khusus User Premium!',
-	wait: 'Loading...',
-	error: 'Error!',
-	done: 'Done'
+	owner: '*Fitur Khusus Owner!*',
+	admin: '*Fitur Khusus Admin!*',
+	botAdmin: '*Bot Bukan Admin!*',
+	group: '*Gunakan Di Group!*',
+	private: '*Gunakan Di Privat Chat!*',
+	prem: '*Khusus User Premium!*',
+	wait: '*Loading...*',
+	error: '*Error!*',
+	done: '*Done*'
 }
 
 global.APIs = {
